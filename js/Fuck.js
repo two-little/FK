a1f706d8a03ceaf1cb0aae18aeae2c82790b310d
two-little <mcpc233@gmail.com>
2019-11-11 01:22:04 +0800

    new4

--- js/Fuck.js
+++ js/Fuck.js
@@ -11,7 +11,7 @@ onload = function () {
 		var osTop = document.documentElement.scrollTop || document.body.scrollTop;
 		console.log(osTop);
 		if (osTop <= 50) {
-			FuckAss.style.top = "90%";
+//			FuckAss.style.top = "90%";
 			FuckYouAvatar.style.width = "200px";
 			FuckYouAvatar.style.height = "200px";
 			FuckYouAvatar.style.top = "calc(0px - (200px / 2))";
@@ -21,7 +21,7 @@ onload = function () {
 		}
 
 		if (osTop > 0) {
-			FuckAss.style.top = "20%";
+//			FuckAss.style.top = "20%";
 			FuckYouAvatar.style.width = "150px";
 			FuckYouAvatar.style.height = "150px";
 			FuckYouAvatar.style.top = "calc(0px - (150px / 2))";
