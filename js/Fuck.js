ab525ab2bfb6f353cbabc653ec98f485c76d8a9c
two-little <mcpc233@gmail.com>
2019-11-12 01:03:15 +0800

    'new5'

--- js/Fuck.js
+++ js/Fuck.js
@@ -1,30 +1,21 @@
 onload = function () {
 	var FuckAss = document.getElementById("FuckAss");
 	var FuckYouAvatar = document.getElementById("FuckYouAvatar");
-	//					var confirms = document.getElementsByClassName("confirm");
-	//					for(var i = 0; i < confirms.length; i++) {
-	//						confirms[i]
-	//					}
-
 
 	window.onscroll = function () {
 		var osTop = document.documentElement.scrollTop || document.body.scrollTop;
 		console.log(osTop);
 		if (osTop <= 50) {
-//			FuckAss.style.top = "90%";
-			FuckYouAvatar.style.width = "200px";
-			FuckYouAvatar.style.height = "200px";
-			FuckYouAvatar.style.top = "calc(0px - (200px / 2))";
-			
-
-
+			$("#FuckYouAvatar").css({
+				'transform':'scale(1.2)'
+			});
 		}
-
 		if (osTop > 0) {
-//			FuckAss.style.top = "20%";
-			FuckYouAvatar.style.width = "150px";
-			FuckYouAvatar.style.height = "150px";
-			FuckYouAvatar.style.top = "calc(0px - (150px / 2))";
+			
+			$("#FuckYouAvatar").css({
+				'transform':'scale(0.8) rotate(360deg)'
+			});
+			
 		}
 	}
 
